dea62aa9f877a222d3d00b3e00d060bcbb11f8eb
Joe Booth <joseph.booth3@gmail.com>
2018-06-08 17:29:28 -0500

    adding friend data

---
+++ app/data/friends.js
@@ -0,0 +1,63 @@
+var friends = [
+    {
+        name: "Jerry Seinfeld",
+        photo: "https://img.wennermedia.com/1200-width/rs-140912-20140627-jerry-x1800-1403899771.jpg",
+        scores: [3, 4, 2, 1, 4, 5, 5, 3, 2, 4]
+    },
+
+    {
+        name: "Cosmo Kramer",
+        photo: "https://img.wennermedia.com/1200-width/rs-140911-20140627-seinfeld-03-x1800-1403889555.jpg",
+        scores: [1, 1, 3, 1, 5, 4, 3, 2, 5, 5]
+    },
+
+    {
+        name: "Elaine Benes",
+        photo: "https://img.wennermedia.com/1200-width/rs-140909-20140627-seinfeld-x1800-1403886665.jpg",
+        scores: [5, 4, 3, 5, 4, 5, 2, 2, 3, 5]
+    },
+
+    {
+        name: "George Costanza",
+        photo: "https://img.wennermedia.com/1200-width/rs-140910-20140627-seinfeld-02-x1800-1403889361.jpg",
+        scores: [2, 2, 1, 5, 4, 5, 2, 4, 5, 5]
+    },
+
+    {
+        name: "Newman",
+        photo: "https://img.wennermedia.com/1200-width/rs-140917-20140630-newman-x1800-1404151263.jpg",
+        scores: [5, 5, 5, 4, 2, 2, 1, 1, 3, 1]
+    },
+
+    {
+        name: "Frank Costanza",
+        photo: "https://img.wennermedia.com/1200-width/rs-140915-20140627-seinfeld-05-x1800-1403894395.jpg", 
+        scores: [3, 1, 3, 5, 5, 5, 1, 4, 5, 3]
+    },
+
+    {
+        name: "Estelle Costanza",
+        photo: "https://img.wennermedia.com/1200-width/rs-140916-20140630-estelle-x1800-1404155907.jpg",
+        scores: [3, 2, 1, 5, 5, 5, 3, 4, 2, 3]
+    }, 
+
+    {
+        name: "Morty Seinfeld",
+        photo: "https://img.wennermedia.com/1200-width/rs-140913-20140627-seinfeld-04-x1800-1403889769.jpg",
+        scores: [2, 2, 5, 4, 5, 5, 4, 3, 4, 2]
+    },
+
+    {
+        name: "Manya",
+        photo: "https://img.wennermedia.com/1200-width/rs-140953-pony1800-1404319931.jpg",
+        scores: [5, 2, 2, 3, 4, 2, 1, 3, 2, 5]
+    },
+
+    {
+        name: "David Puddy",
+        photo: "https://img.wennermedia.com/1200-width/rs-140932-20140630-puddy-x1800-1404151793.jpg",
+        scores: [1, 3, 1, 3, 5, 5, 2, 5, 3, 1]
+    }
+]
+
+module.exports = friends;
