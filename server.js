0efe39b2081f745056c31408cc0ab9ed1c89cb43
Joe Booth <joseph.booth3@gmail.com>
2018-06-08 08:12:14 -0500

    creating file structure and adding home.html

---
+++ server.js
@@ -0,0 +1,11 @@
+//Dependencies
+var express = require("express");
+var bodyParser = require('body-parser');
+var path = require('path');
+
+// Create express app instance.
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
