189a3cf93e2d6f6510a70b0cf79ba87c68a24841
Joe Booth <joseph.booth3@gmail.com>
2018-06-11 07:07:25 -0500

    setting up server.js connection

--- server.js
+++ server.js
@@ -9,3 +9,23 @@ var app = express();
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
+
+app.get('/', function (req, res) {
+    res.send("Hello World");
+})
+
+//parse application/ url encoded
+app.use(bodyParser.urlencoded({ extended: false}))
+
+//parse application/json
+app.use(bodyParser.json())
+
+app.use(function (req, res) {
+    res.setHeader("Content-Type", "text/plain")
+    res.write("you posted:\n")
+    res.end(JSON.stringify(req.body, null, 2))
+})
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+})
